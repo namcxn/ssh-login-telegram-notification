a84fdfce7bc8284f59fb00d4ccf0740b0317504f
Cao Xuân Nam <ghn.tech.namcao@gmail.com>
2018-06-15 15:00:03 +0700

    first

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # ssh-login-telegram-notification
+### Use with PAM
+###### Setup a Telegram bot
+First of all, you’ll need to have a Telegram account and create a Telegram bot
+###### Script to send message via the bot
+Put file ssh-telegram.sh to /usr/local/bin/
+> Remember to replace <your bot token> and <your chat ID> with the real data of your own.
+###### When a user logs in on the server
+* With Debian/Ubuntu
+Edit file /etc/pam.d/common-session
+> session optional pam_exec.so type=open_session seteuid /usr/local/bin/ssh-telegram.sh
+* With CentOS
+Edit the file /etc/pam.d/system-auth
+> session optional pam_exec.so type=open_session seteuid /usr/local/bin/tgbot.sh
+
+### Put the command to run in the user’s shell init script, such as .bashrc, or /etc/profile or a script under /etc/profile.d/.
---
+++ ssh-telegram.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+KEY="<your telgram token>"
+URL="https://api.telegram.org/bot$KEY/sendMessage"
+TARGET="<yout telegram chat ID>" # Telegram ID of the conversation with the bot, get it from /getUpdates API
+
+DATE_EXEC="$(date "+%d-%b-%Y-%H:%M")"
+TMPFILE=/tmp/ipinfo-${DATE_EXEC}.txt
+
+curl http://ipinfo.io/$PAM_RHOST -s -o $TMPFILE
+CITY=$(cat $TMPFILE | jq '.city' | sed 's/"//g')
+REGION=$(cat $TMPFILE | jq '.region' | sed 's/"//g')
+COUNTRY=$(cat $TMPFILE | jq '.country' | sed 's/"//g')
+ORG=$(cat $TMPFILE | jq '.org' | sed 's/"//g')
+
+TEXT="User *$PAM_USER* logged in on *$HOSTNAME* at $(date '+%Y-%m-%d %H:%M:%S %Z') from $PAM_RHOST - $ORG - $CITY, $REGION, $COUNTRY"
+#Remote host: $PAM_RHOST"
+
+PAYLOAD="chat_id=$TARGET&text=$TEXT&parse_mode=Markdown&disable_web_page_preview=true"
+
+# Run in background so the script could return immediately without blocking PAM
+curl -s --max-time 10 --retry 5 --retry-delay 2 --retry-max-time 10 -d "$PAYLOAD" $URL > /dev/null 2>&1 &
